d917188dd1818e8306be62fbc4a86a31e9bfc396
Sobka <bogdannn55rus@gmail.com>
2024-06-27 22:08:37 +0500

    а
    а

--- searchindex.js
+++ searchindex.js
@@ -16,7 +16,7 @@ function SearchDatabase()
    this[database_length++] = new SearchPage("index.html", "BogdanSite2014", " ", "");
    this[database_length++] = new SearchPage("ПОБЕГ-ОТ-СОБКЕ.html", "ПОБЕГ ОТ СОБКИ", "1 ЧАСТЬ  1 СЕРИЯ  2 СЕРИЯ  ТРЕЙЛЕРЫ   ", "");
    this[database_length++] = new SearchPage("Social.html", "BogdanSite2014 Social", " ", "");
-   this[database_length++] = new SearchPage("page1.html", "YOU ARE AN IDIOT!!!", " ", "");
+   this[database_length++] = new SearchPage("YOU-ARE-AN-IDIOT!!!.html", "YOU ARE AN IDIOT!!!", " ", "");
    this[database_length++] = new SearchPage("404.html", "404", "Упсс! Вы попали на страницу 404!  Но можете здесь посмотреть флешку   ЧТОБЫ ФЛЕШ ЗАРАБОТАЛ СКАЧАЙТЕ  https //chromewebstore.google.com/detail/ruffle-flash-emulator/donbcfbmhbcapadipfkeojnmajbakjdc?hl=ru  На главную   ", "");
    return this;
 }
