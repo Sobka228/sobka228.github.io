6f6d2a52c2ad86ba2c42773f4d2b6399efb07623
Sobka <bogdannn55rus@gmail.com>
2024-07-03 13:18:31 +0500

    frefe
    gergtergterg4

--- searchindex.js
+++ searchindex.js
@@ -13,11 +13,13 @@ function SearchPage(url, title, keywords, description)
 function SearchDatabase()
 {
    database_length = 0;
-   this[database_length++] = new SearchPage("index.html", "BogdanSite2014", " Мой Youtube    ПОБЕГ ОТ СОБКЕ    Мой ТГК    Sobka Coin Web   Кто я?  Обычный челик который создает контент на ютубе или создаю модельки или что-то другое О сайте   Новости или какой-то блог с приколюхами и с приколами   Старый Сайт   Добавляй наш баннер на свой сайт! Мне будет приятно  3  Баннер будет выглядить вот так  ЕСЛИ У ЮЗЕРОВ ЯНДЕКСА ИЛИ SAFARI НЕ РАБОТАЕТ SHOUTBOX ТО ЗАПУСТИТЕ EDGE ЛИБО ДРУГОЙ БРАУЗЕР   ", "");
+   this[database_length++] = new SearchPage("index.html", "BogdanSite2014", " Мой Youtube    ПОБЕГ ОТ СОБКЕ    Мой ТГК    Sobka Coin Web    Старый Сайт   ЕСЛИ У ЮЗЕРОВ ЯНДЕКСА ИЛИ SAFARI НЕ РАБОТАЕТ SHOUTBOX ТО ЗАПУСТИТЕ EDGE ЛИБО ДРУГОЙ БРАУЗЕР  Добавляй наш баннер на свой сайт! Мне будет приятно  3  Баннер будет выглядить вот так  Кто я?  Обычный челик который создает контент на ютубе или создаю модельки или что-то другое О сайте   Новости или какой-то блог с приколюхами и с приколами   ", "");
    this[database_length++] = new SearchPage("ПОБЕГ-ОТ-СОБКЕ.html", "ПОБЕГ ОТ СОБКИ", "1 ЧАСТЬ  1 СЕРИЯ  2 СЕРИЯ  ТРЕЙЛЕРЫ  3 СЕРИЯ  4 СЕРИЯ  5 СЕРИЯ   ", "");
    this[database_length++] = new SearchPage("YOU-ARE-AN-IDIOT!!!.html", "YOU ARE AN IDIOT!!!", " ", "");
    this[database_length++] = new SearchPage("404.html", "404", "Упсс! Вы попали на страницу 404!  Но можете здесь посмотреть флешку   ЧТОБЫ ФЛЕШ ЗАРАБОТАЛ СКАЧАЙТЕ  https //chromewebstore.google.com/detail/ruffle-flash-emulator/donbcfbmhbcapadipfkeojnmajbakjdc?hl=ru  На главную   ", "");
    this[database_length++] = new SearchPage("500.html", "500", "ОШИБКА 500  500 Internal Server Error   ", "");
    this[database_length++] = new SearchPage("405.html", "405", "Упсс! Вы попали на страницу 405!  Но можете здесь посмотреть флешку   ЧТОБЫ ФЛЕШ ЗАРАБОТАЛ СКАЧАЙТЕ  https //chromewebstore.google.com/detail/ruffle-flash-emulator/donbcfbmhbcapadipfkeojnmajbakjdc?hl=ru   ", "");
+   this[database_length++] = new SearchPage("old-version.html", "BogdanSite2014", " Мой Youtube    ПОБЕГ ОТ СОБКЕ    Мой ТГК    Sobka Coin Web    Старый Сайт   ЕСЛИ У ЮЗЕРОВ ЯНДЕКСА ИЛИ SAFARI НЕ РАБОТАЕТ SHOUTBOX ТО ЗАПУСТИТЕ EDGE ЛИБО ДРУГОЙ БРАУЗЕР  Добавляй наш баннер на свой сайт! Мне будет приятно  3  Баннер будет выглядить вот так  Кто я?  Обычный челик который создает контент на ютубе или создаю модельки или что-то другое О сайте   Новости или какой-то блог с приколюхами и с приколами   ", "");
+   this[database_length++] = new SearchPage("ПОБЕГ-ОТ-СОБКЕ-old.html", "ПОБЕГ ОТ СОБКИ", "1 ЧАСТЬ  1 СЕРИЯ  2 СЕРИЯ  ТРЕЙЛЕРЫ  3 СЕРИЯ  4 СЕРИЯ  5 СЕРИЯ   ", "");
    return this;
 }
